c1424d6d7b1ef2256263b7cc6138d61161347104
CaecharoDP <caecharowork@gmail.com>
2025-12-18 03:52:43 +0700

    Import libraries and load data in model_training.py
    Added data loading section to the model training script.

---
+++ notebooks/model_training.py
@@ -0,0 +1,19 @@
+# Import Required Libraries
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+import seaborn as sns
+
+# Load the Data
+
+url="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/_0eYOqji3unP1tDNKWZMjg/weatherAUS-2.csv"
+df = pd.read_csv(url)
+df.head()
+
